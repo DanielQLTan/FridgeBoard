6274ae87afa3059720636f5db16945f22f381c31
Siruiruirui <sirui.li.contact@gmail.com>
2025-05-01 14:28:42 -0700

    change color when active

--- js/navbar.js
+++ js/navbar.js
@@ -9,11 +9,25 @@ document.addEventListener('DOMContentLoaded', function() {
   // Set the active state based on current page
   navItems.forEach((item) => {
     const itemHref = item.getAttribute('href');
+    const icon = item.querySelector('.nav-icon');
+    
+    // Get the current image source and its name
+    const currentSrc = icon.getAttribute('src');
+    const imgPath = currentSrc.substring(0, currentSrc.lastIndexOf('/') + 1);
+    const imgName = currentSrc.substring(currentSrc.lastIndexOf('/') + 1);
+    const baseName = imgName.replace('-active.svg', '').replace('.svg', '');
+    
     if (currentPath.includes(itemHref)) {
       // Add active class
       item.classList.add('active');
+      
+      // Use the active version of the icon
+      icon.setAttribute('src', `${imgPath}${baseName}-active.svg`);
     } else {
       item.classList.remove('active');
+      
+      // Use the regular version of the icon
+      icon.setAttribute('src', `${imgPath}${baseName}.svg`);
     }
   });
 }); 
