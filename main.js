c9a4a2042a10de4e57ec8fa2da25fc6384feddce
Daniel Tan <daniel_tan@berkeley.edu>
2025-05-06 16:33:35 -0700

    setup pages

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ function nav() {
 				<img class="nav-icon" src="../img/calendar.svg" alt="Expiration">
 				<div class="nav-text">Track</div>
 			</a>
-			<a href="#" class="nav-item">
+			<a href="###" class="nav-item">
 				<img class="nav-icon" src="../img/cart.svg" alt="Shopping">
 				<div class="nav-text">Shop</div>
 			</a>
-			<a href="#" class="nav-item">
+			<a href="###" class="nav-item">
 				<img class="nav-icon" src="../img/user.svg" alt="Profile">
 				<div class="nav-text">Me</div>
 			</a>
@@ -28,9 +28,7 @@ function nav() {
 function bar() {
 	const items = document.querySelectorAll('.nav-item');
 	for (item of items) {
-		console.log(item.getAttribute('href'))
-		console.log(window.location.pathname)
-		if (item.getAttribute('href').includes(window.location.pathname)) {
+		if (window.location.pathname.includes(item.getAttribute('href').slice(2))) {
 			item.classList.add('active');
 			const icon = item.querySelector('.nav-icon');
 			const src = icon.getAttribute('src').replace('.svg', '-active.svg');
